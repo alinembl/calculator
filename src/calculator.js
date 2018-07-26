963d63ebe4676238b06d6371b313fc19d142a5a9
Aline Bispo <alinembispolima@gmail.com>
2018-07-26 17:46:38 -0300

    first commit

---
+++ src/calculator.js
@@ -0,0 +1,7 @@
+export const sum = (x, y) => x + y
+
+export const subtract = (x, y) => x - y
+
+export const multiply = (x, y) => x * y
+
+export const divide = (x, y) => x / y
---
+++ test/calculator.test.js
@@ -0,0 +1,45 @@
+import chai from 'chai'
+import { sum } from '../src/calculator'
+import { subtract } from '../src/calculator'
+import { multiply } from '../src/calculator'
+import { divide } from '../src/calculator'
+
+const expect = chai.expect
+
+describe('calculator', () => {
+  describe('sum', () => {
+    it('is a function', () => {
+      expect(sum).to.be.a('function')
+    })
+    it('sums two numbers', () => {
+      expect(sum(1, 2)).to.equal(3)
+    })
+  })
+
+  describe('subtract', ()=> {
+    it('is a function', () => {
+      expect(subtract).to.be.a('function')
+    })
+    it('subtracts two numbers', () =>{
+      expect(subtract(2, 1)).to.equal(1)
+    })
+  })
+
+ describe('multiply', () => {
+   it('is a function', () => {
+     expect(multiply).to.be.a('function')
+   })
+   it('multiplies two numbers', ()=> {
+     expect(multiply(3, 5)).to.equal(15)
+   })
+ })
+ describe('divide', () => {
+   it('is a function', () => {
+     expect(divide).to.be.a('function')
+   })
+   it('divides two numbers', () =>{
+     expect(divide(25, 5)).to.equal(5)
+   })
+ })
+
+})
