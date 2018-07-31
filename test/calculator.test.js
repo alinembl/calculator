3ef189cd6ed56e045417f1f78d6e53a460cf8d92
Aline Bispo <alinembispolima@gmail.com>
2018-07-31 09:53:34 -0300

    Adds lint in calculator project

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,11 +1,9 @@
 import chai from 'chai'
-import { sum } from '../src/calculator'
-import { subtract } from '../src/calculator'
-import { multiply } from '../src/calculator'
-import { divide } from '../src/calculator'
+import { sum, subtract, multiply, divide } from '../src/calculator'
 
 const expect = chai.expect
 
+/* eslint-env mocha */
 describe('calculator', () => {
   describe('sum', () => {
     it('is a function', () => {
@@ -15,31 +13,29 @@ describe('calculator', () => {
       expect(sum(1, 2)).to.equal(3)
     })
   })
-
-  describe('subtract', ()=> {
+  describe('subtract', () => {
     it('is a function', () => {
       expect(subtract).to.be.a('function')
     })
-    it('subtracts two numbers', () =>{
+    it('subtracts two numbers', () => {
       expect(subtract(2, 1)).to.equal(1)
     })
   })
 
- describe('multiply', () => {
-   it('is a function', () => {
-     expect(multiply).to.be.a('function')
-   })
-   it('multiplies two numbers', ()=> {
-     expect(multiply(3, 5)).to.equal(15)
-   })
- })
- describe('divide', () => {
-   it('is a function', () => {
-     expect(divide).to.be.a('function')
-   })
-   it('divides two numbers', () =>{
-     expect(divide(25, 5)).to.equal(5)
-   })
- })
-
+  describe('multiply', () => {
+    it('is a function', () => {
+      expect(multiply).to.be.a('function')
+    })
+    it('multiplies two numbers', () => {
+      expect(multiply(3, 5)).to.equal(15)
+    })
+  })
+  describe('divide', () => {
+    it('is a function', () => {
+      expect(divide).to.be.a('function')
+    })
+    it('divides two numbers', () => {
+      expect(divide(25, 5)).to.equal(5)
+    })
+  })
 })
